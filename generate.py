b8f8aca839da293f74b9f57dcbbea8a78e418243
FrBarbarossa <91803857+FrBarbarossa@users.noreply.github.com>
2022-09-10 15:53:14 +0300

    Обновлённая версия
    Вычищен код, добавлены разделы в --help
    Исправлена ошибка при падении генерации
    Изменён алгоритм генерации для получения более разнообразных текстов
    Сокращено время генерации при большой обученной модели.
    Добавлен "лишний" вывод в консоль для упрощения отлова ошибок (На случай, если у файла сломанная кодировка)

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-import pprint
 import re
 import os
 import json
@@ -9,21 +8,21 @@ import sys
 
 class Ngrams:
     def __init__(self):
-        if 'model.json' not in os.listdir():
-            self.iternal_start()
         self.tmp = {}
 
     def generate(self, text=None, length=0):
         self.get_data()
+        if not text:
+            text = random.choice(list(self.tmp['4'].keys()))
         phrase = self.tokenization(txt=text)[1]
         current_length = len(phrase)
         while current_length < length:
             found = False
-            part_length = 5 if current_length >= 5 else current_length
+            part_length = random.choice([2, 3, 4, 5]) if current_length >= 5 else random.choice(
+                range(1, current_length + 1))
             while part_length >= 1 and not found:
                 tail = ' '.join(phrase[-part_length:])
                 if tail in self.tmp[str(part_length)].keys():
-                    # print(part_length, phrase, list(self.tmp[str(part_length)][tail].items()))
                     variants = sorted(list(self.tmp[str(part_length)][tail].items()), key=lambda x: -x[1])
                     phrase.append(variants[0][0])
                     current_length += 1
@@ -34,30 +33,11 @@ class Ngrams:
                 if not found:
                     phrase.append(random.choice(list(self.tmp['1'].keys())))
                     current_length += 1
-        print(phrase)
-
-    '''Возвращает словарь с уже имеющимися данными'''
+        print(' '.join(phrase))
 
     def get_data(self):
         out = open('model.json', mode='r')
         self.tmp = dict(json.load(out))
-
-    '''На случай, если нет изначально обученной модели - создаёт каркас в формате JSON'''
-
-    def iternal_start(self):
-        initial_data = {
-            'books': [],
-            1: {},
-            2: {},
-            3: {},
-            4: {},
-            5: {}
-        }
-        file = open('model.json', mode='w')
-        json.dump(initial_data, file)
-        file.close()
-
-    '''Разбивает полученный файл/текст на токены'''
 
     def tokenization(self, filename=None, txt=None, n=1):
         if filename:
@@ -67,27 +47,25 @@ class Ngrams:
         raw_tokens = list(
             i for i in map(lambda i: i.lower() if i.isalpha() else '', re.split(r'\W+', txt)) if i.isalpha())
         tokens = list(zip(*[raw_tokens[i:] for i in range(n)]))
-        # print(tokens)
         return tokens, raw_tokens
 
 
 if __name__ == '__main__':
     sample = Ngrams()
-    # sample.iternal_start()
-    # sample.tokenization(filename='data/Мастер и Маргарита.txt', n=6)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--model',
         action='store_const',
         const=True,
-        default=False
+        default=False,
+        help='To explore path to model'
     )
     parser.add_argument(
         '--prefix',
         nargs='+',
         type=str,
         required=False,
-        help='path to text to fit (default stdin)'
+        help='Enter the start of phrase (uses as a seed)'
     )
 
     parser.add_argument(
@@ -99,20 +77,12 @@ if __name__ == '__main__':
     )
 
     my_namespace = parser.parse_args()
-    # print(sys.argv[1:])
-    # print(my_namespace.input_dir)
     phrase_length = my_namespace.length[0]
     if my_namespace.model:
         print('PATH of model: ' + os.path.abspath('model.json'))
     if my_namespace.prefix:
         text = ' '.join(my_namespace.prefix)
         sample.generate(text=text, length=phrase_length)
-        # print(text, phrase_length)
-        # sample.fit(file=data)
-        # print('!!', data)
     else:
         text = ''
         sample.generate(text=text, length=phrase_length)
-        # sample.fit(text=text)
-        # stdin = sys.stdin.readlines()
-        # print(stdin)
--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import pprint
 import re
 import os
 import json
@@ -9,7 +8,7 @@ import sys
 class Ngrams:
     def __init__(self):
         if 'model.json' not in os.listdir():
-            self.iternal_start()
+            self.__iternal_start()
         self.tmp = {}
 
     def fit(self, text=None, file=None):
@@ -17,6 +16,7 @@ class Ngrams:
         if file:
             files = [i for i in os.listdir(file)]
             for path in files:
+                print("Читаю книгу:..", path)
                 self.make_corpus(path=file + '\\' + path)
         if text:
             self.make_corpus(text=text)
@@ -30,42 +30,26 @@ class Ngrams:
             if path:
                 tokens = self.tokenization(filename=path, n=token_length)
 
-            if token_length == 1:
-                if tokens[1] not in self.tmp['books']:
-                    self.tmp['books'].append(tokens[1])
-            # if token_length == 2:
-            #     print(token_length, tokens[0])
             self.update_data(tokens=tokens[0], length=str(token_length))
 
     def update_data(self, tokens, length):
-        # print(tokens)
-        for i in range(len(tokens)-1):
-            # print(tokens[i], tokens[i+1])
+        for i in range(len(tokens) - 1):
             current_word = ' '.join(tokens[i])
-            # print('!!!', current_word)
-            # print('!!!!', tokens)
             if current_word:
-                if current_word not in self.tmp[length].keys():
-                    self.tmp[length][current_word] = {}
                 next_word = tokens[i + 1][-(1 % int(length))]
-                # print(current_word, next_word, tokens[i+1], 1%int(length), length)
                 if next_word != tokens[i][-1]:
+                    if current_word not in self.tmp[length].keys():
+                        self.tmp[length][current_word] = {}
                     if next_word not in self.tmp[length][current_word].keys():
                         self.tmp[length][current_word][next_word] = 0
                     self.tmp[length][current_word][next_word] += 1
-                    # print(current_word, next_word, self.tmp[length][current_word])
-
-    '''Возвращает словарь с уже имеющимися данными'''
 
     def get_data(self):
         out = open('model.json', mode='r')
         self.tmp = dict(json.load(out))
 
-    '''На случай, если нет изначально обученной модели - создаёт каркас в формате JSON'''
-
-    def iternal_start(self):
+    def __iternal_start(self):
         initial_data = {
-            'books': [],
             1: {},
             2: {},
             3: {},
@@ -76,8 +60,6 @@ class Ngrams:
         json.dump(initial_data, file)
         file.close()
 
-    '''Разбивает полученный файл/текст на токены'''
-
     def tokenization(self, filename=None, txt=None, n=1):
         if filename:
             file = open(filename, 'rt')
@@ -85,46 +67,37 @@ class Ngrams:
             file.close()
         raw_tokens = list(
             _ for _ in map(lambda i: i.lower() if i.isalpha() else '', re.split(r'\W+', txt)) if _.isalpha())
-        # print(raw_tokens)
         tokens = list(zip(*[raw_tokens[i:] for i in range(n)]))
-        # print('!!!', n, tokens)
-        # print(tokens)
         return tokens, raw_tokens
 
 
 if __name__ == '__main__':
     sample = Ngrams()
-    # sample.iternal_start()
-    # sample.tokenization(filename='data/Мастер и Маргарита.txt', n=6)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--model',
         action='store_const',
         const=True,
-        default=False
+        default=False,
+        help='To explore path to model'
     )
     parser.add_argument(
         '--input-dir',
         nargs='+',
         type=str,
         required=False,
-        help='path to text to fit (default stdin)'
+        help='Use --input-dir PATH/FILE to enter directory where you put .txt files to train model. \n If you doesn`t use --input-dir, you may enter text in console (stdin)'
     )
 
     my_namespace = parser.parse_args()
-    # print(sys.argv[1:])
-    # print(my_namespace.input_dir)
     if my_namespace.model:
         print('PATH of model: ' + os.path.abspath('model.json'))
     if my_namespace.input_dir:
-        # print(my_namespace.input_dir)
         data = ' '.join(my_namespace.input_dir)
         sample.fit(file=data)
-        # print('!!', data)
     else:
         text = ''
+        print("Введите текст ниже: ")
         for i in sys.stdin.readlines():
             text += i
         sample.fit(text=text)
-        # stdin = sys.stdin.readlines()
-        # print(stdin)
